c9da120f78be59893719e572014281d81531af3f
ddt-yamamoto <ddt-yamamoto@gmail.com>
2018-10-04 20:38:44 +0900

    佐和さんからの第一次指摘箇所を修正

--- 6章/06-11.cpp
+++ 6章/06-11.cpp
@@ -15,121 +15,125 @@ x - y - z
 using namespace std;
 
 /**
-3桁の正の整数を乱数で生成し、それらを用いた4種の計算問題のいずれかをランダムで出題する関数
+受け取った引数に応じて、以下の4種のいずれかの演算を行った結果と、入力値とを比較して、正解か不正解かを返す関数
 x + y + z
 x + y - z
 x - y + z
 x - y - z
-@param なし
+@param argumentRandomX 計算式中のxに相当するオペランド
+@param argumentRandomY 計算式中のyに相当するオペランド
+@param argumentRandomZ 計算式中のzに相当するオペランド
+@param argumentRandomDrillType 4種の演算子組み合わせタイプを決定する
 @return なし
 @author Kenta Yamamoto
 @since 2018-10-02
 */
-void randomCalculationDrill() {
-
-	// 乱数の種を生成
-	srand(time(NULL));
-
-	// 100〜999の乱数を3つ定義
-	int randomValueX = rand() % 900 + 100; // x
-	int randomValueY = rand() % 900 + 100; // y
-	int randomValueZ = rand() % 900 + 100; // z
-
-	// 出題タイプをランダムに決めるための0〜3の乱数を定義
-	int randomDrillType = rand() % 4;
-
-	// 問題文の、出題タイプによって切り替わる部分の演算子をchar型で宣言
-	char operand1st; // 一つ目の演算子
-	char operand2nd; // 二つ目の演算子
+bool answerCheck(int argumentRandomX, int argumentRandomY, int argumentRandomZ, int argumentRandomDrillType) {
 
 	// 正答を計算して格納するための変数を宣言
 	int correctAnswer;
 
+	// 入力値を格納するための変数を宣言
+	int inputAnswer;
+
 	// 乱数により出題が4種類に分岐
-	switch (randomDrillType) {
+	switch (argumentRandomDrillType) {
 
 	// x + y + zの場合
 	case 0:
-
-		// 出題文を切り替えるためにchar型の演算子を代入
-		operand1st = '+';	// 第一オペランドが+
-		operand2nd = '+';	// 第二オペランドが+
-
 		// 正答を計算して変数に格納しておく
-		correctAnswer = randomValueX + randomValueY + randomValueZ;
-
+		correctAnswer = argumentRandomX + argumentRandomY + argumentRandomZ;
 		// switchを抜ける
 		break;
 
 	// x + y - zの場合
 	case 1:
-
-		// 出題文を切り替えるためにchar型の演算子を代入
-		operand1st = '+';	// 第一オペランドが+
-		operand2nd = '-';	// 第二オペランドが-
-
 		// 正答を計算して変数に格納しておく
-		correctAnswer = randomValueX + randomValueY - randomValueZ;
-
+		correctAnswer = argumentRandomX + argumentRandomY - argumentRandomZ;
 		// switchを抜ける
 		break;
 
-		// x - y + zの場合
+	// x - y + zの場合
 	case 2:
-
-		// 出題文を切り替えるためにchar型の演算子を代入
-		operand1st = '-';	// 第一オペランドが-
-		operand2nd = '+';	// 第二オペランドが+
-
 		// 正答を計算して変数に格納しておく
-		correctAnswer = randomValueX - randomValueY + randomValueZ;
-
+		correctAnswer = argumentRandomX - argumentRandomY + argumentRandomZ;
 		// switchを抜ける
 		break;
 
-		// x - y - zの場合
+	// x - y - zの場合
 	case 3:
-
-		// 出題文を切り替えるためにchar型の演算子を代入
-		operand1st = '-';	// 第一オペランドが-
-		operand2nd = '-';	// 第二オペランドが-
-
 		// 正答を計算して変数に格納しておく
-		correctAnswer = randomValueX - randomValueY - randomValueZ;
-
+		correctAnswer = argumentRandomX - argumentRandomY - argumentRandomZ;
 		// switchを抜ける
 		break;
 
 	// それ以外の場合
 	default:
-
 		// switchを抜ける
 		break;
 	}
 
-	// 正解してbreakで抜けるまでループ
-	while (true) {
+	// 入力値を変数に格納
+	cin >> inputAnswer;
 
-		// 回答の入力を格納する変数を宣言
-		int inputAnswer;
+	// 正解と比較した結果で、返却用bool型変数を初期化
+	bool returnBool = (inputAnswer == correctAnswer ? true : false);
 
-		// 出題メッセージ
-		cout << randomValueX << ' ' << operand1st << ' '
-			<< randomValueY << ' ' << operand2nd << ' '
-			<< randomValueZ << " = ";
+	// bool型変数を返却
+	return returnBool;
+}
 
-		// 入力値を格納
-		cin >> inputAnswer;
+/**
+*受け取った引数に応じて、以下の4種のいずれかの演算を出題する文字列を出力する関数
+*x + y + z
+*x + y - z
+*x - y + z
+*x - y - z
+*@param argumentRandomX 計算式中のxに相当するオペランド
+*@param argumentRandomY 計算式中のyに相当するオペランド
+*@param argumentRandomZ 計算式中のzに相当するオペランド
+*@param argumentRandomDrillType 4種の演算子組み合わせタイプを決定する
+*@return なし
+*@author Kenta Yamamoto
+*@since 2018-10-02
+*/
+void putQuiz(int argumentRandomX, int argumentRandomY, int argumentRandomZ, int argumentRandomDrillType) {
 
-		// 入力値とあらかじめ用意しておいた正答とを比較
-		if (inputAnswer == correctAnswer) {
+	// 乱数により出題が4種類に分岐
+	switch (argumentRandomDrillType) {
 
-			// 正解ならwhile文を抜ける
-			break;
-		}
+	// x + y + zの場合
+	case 0:
+		// 問題文を出力
+		cout << argumentRandomX << " + " << argumentRandomY << " + " << argumentRandomZ << " = ";
+		// switchを抜ける
+		break;
 
-		// 間違っていたらここにくるので、間違いメッセージを出力
-		cout << "違いますよ！ｗ\n";
+	// x + y - zの場合
+	case 1:
+		// 問題文を出力
+		cout << argumentRandomX << " + " << argumentRandomY << " - " << argumentRandomZ << " = ";
+		// switchを抜ける
+		break;
+
+	// x - y + zの場合
+	case 2:
+		// 問題文を出力
+		cout << argumentRandomX << " - " << argumentRandomY << " + " << argumentRandomZ << " = ";
+		// switchを抜ける
+		break;
+
+	// x - y - zの場合
+	case 3:
+		// 問題文を出力
+		cout << argumentRandomX << " - " << argumentRandomY << " - " << argumentRandomZ << " = ";
+		// switchを抜ける
+		break;
+
+	// それ以外の場合
+	default:
+		// switchを抜ける
+		break;
 	}
 }
 
@@ -166,8 +170,31 @@ int main() {
 
 	// 続行確認で続けるを選ぶ限り、続ける
 	do {
-		// 問題出題用の関数を呼ぶ
-		randomCalculationDrill();
+
+		// 乱数の種を生成
+		srand(time(NULL));
+
+		// 100〜999の乱数を3つ定義
+		int randomValueX = rand() % 900 + 100; // x
+		int randomValueY = rand() % 900 + 100; // y
+		int randomValueZ = rand() % 900 + 100; // z
+
+		// 出題タイプをランダムに決めるための0〜3の乱数を定義
+		int randomDrillType = rand() % 4;
+
+		while (true) {
+
+			// 問題文を出力するための関数を呼ぶ
+			putQuiz(randomValueX, randomValueY, randomValueZ, randomDrillType);
+
+			// 入力を受け付け、計算を行い、それらを比較して正解かを判定する関数を呼び、正解であればwhileループを抜ける
+			if (answerCheck(randomValueX, randomValueY, randomValueZ, randomDrillType)) {
+				break;
+			}
+
+			// 間違っていたらここにくるので、間違いメッセージを出力
+			cout << "違いますよ！\n";
+		}
 
 	// 続行確認の関数を呼び出し、ループを終了するか判定
 	} while (confirmRetry());
