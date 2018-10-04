00eaa356a36c1c1c1c336af335c3ba616cbb87ce
ddt-yamamoto <ddt-yamamoto@gmail.com>
2018-10-04 20:37:40 +0900

    佐和さんからの第一次指摘箇所を修正

--- 6章/06-09.cpp
+++ 6章/06-09.cpp
@@ -14,25 +14,25 @@ using namespace std;
 
 /**
 a以上b未満の乱数を生成して、その値を返却する関数
-@param argumentInt1st aにあたる整数で、乱数の下限、
-@param argumentInt2nd bにあたる整数で、乱数の上限(bは含まない)
+@param argumentLowerLimit aにあたる整数で、乱数の下限、
+@param argumentUpperLimit bにあたる整数で、乱数の上限(bは含まない)
 @return returnRandomInt 生成した乱数、もしくはaそのもの(a>=bのとき)
 @author Kenta Yamamoto
 @since 2018-10-02
 */
-int getRamdomInt(int argumentInt1st, int argumentInt2nd) {
+int getRamdomInt(int argumentLowerLimit, int argumentUpperLimit) {
 
 	// 返却値を定義。生成した乱数を格納する
 	int returnRandomInt = 0;
 
 	// 引数の差をとり、変数の初期化子にする
-	int differenceInt = argumentInt2nd - argumentInt1st;
+	int differenceInt = argumentUpperLimit - argumentLowerLimit;
 
 	// bがa以下だったとき
 	if (differenceInt <= 0) {
 
 		// 引数aをそのまま返却する
-		returnRandomInt = argumentInt1st;
+		returnRandomInt = argumentLowerLimit;
 	}
 
 	// 正しく(a < b)だったとき
@@ -42,7 +42,7 @@ int getRamdomInt(int argumentInt1st, int argumentInt2nd) {
 		srand(time(NULL));
 
 		// 乱数を生成し引数の差の剰余をとり、そこに小さいほうの引数(a)を加えると、a以上b未満の乱数ができる
-		returnRandomInt = rand() % differenceInt + argumentInt1st;
+		returnRandomInt = rand() % differenceInt + argumentLowerLimit;
 	}
 
 	// 乱数(もしくは引数aそのまま)を返却
@@ -53,8 +53,8 @@ int getRamdomInt(int argumentInt1st, int argumentInt2nd) {
 int main() {
 
 	// 入力値を格納する変数を定義
-	int inputInt1st = 0; // 一つ目(a)
-	int inputInt2nd = 0; // 二つ目(b)
+	int inputLowerLimit = 0; // 一つ目(a)
+	int inputUpperLimit = 0; // 二つ目(b)
 
 	// 説明メッセージを出力
 	cout << "整数a以上整数b未満の乱数を生成して表示します。\n";
@@ -63,14 +63,14 @@ int main() {
 	cout << "整数a:";
 
 	// 入力値を変数に格納
-	cin >> inputInt1st;
+	cin >> inputLowerLimit;
 
 	// 入力を促す
 	cout << "整数b:";
 
 	// 入力値を変数に格納
-	cin >> inputInt2nd;
+	cin >> inputUpperLimit;
 
 	// 乱数の上限と下限を関数に渡し、返却値の乱数を表示
-	cout << getRamdomInt(inputInt1st, inputInt2nd);
+	cout << getRamdomInt(inputLowerLimit, inputUpperLimit);
 }
